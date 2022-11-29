b6d5e03b97be437c135f8475fd6ffa62bb58a493
ncaillin <nugentcaillin@gmail.com>
2022-11-29 18:09:42 +1100

    3.22

--- notes backend/models/person.js
+++ notes backend/models/person.js
@@ -5,41 +5,41 @@ const url = process.env.MONGODB_URL
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(
-        result => {
-            console.log('connected to MongoDB')
-        }
-    )
-    .catch((error) => {
-        console.log('error connecting to MongoDB', error.message)
-    })
+  .then(
+    () => {
+      console.log('connected to MongoDB')
+    }
+  )
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            minLength: 3,
-            required: true
+  {
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      validate: {
+        validator: v => {
+          return /^\d{2,3}(-)\d*$/.test(v)
         },
-        number: {
-            type: String,
-            validate: {
-                validator: v => {
-                    return /^\d{2,3}(-)\d*$/.test(v)
-                },
-                message: 'number not in valid format'
-            },
-            required: true
-        }
+        message: 'number not in valid format'
+      },
+      required: true
     }
+  }
 )
 
 personSchema.set('toJSON' , {
-    transform: (document, returnedObj) => {
-        returnedObj.id = document._id.toString()
-        delete returnedObj._id
-        delete returnedObj.__v
-    }
+  transform: (document, returnedObj) => {
+    returnedObj.id = document._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+  }
 })
 
 
